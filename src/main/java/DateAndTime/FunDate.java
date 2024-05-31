56f0e6d3f9278034e9b2563c2c90741d254e09db
Oliver Consterla Araya <oliver_consterla@yahoo.cl>
2024-05-31 16:46:23 -0400

    chore: Update dependencies in pom.xml and refactor FunDate class

--- src/main/java/DateAndTime/FunDate.java
+++ src/main/java/DateAndTime/FunDate.java
@@ -1,10 +1,10 @@
 package DateAndTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -14,16 +14,51 @@ import java.util.Date;
  * @version 1.0                                                               *
  * @since 2024                                                                *
  ******************************************************************************/
+@Slf4j
 public class FunDate {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FunDate.class.getName());
-
-    public synchronized Date parsero(SimpleDateFormat parser, String hora){
+    public synchronized Date StringToDate(String pattern, String fechaHora){
+        SimpleDateFormat parser = new SimpleDateFormat(pattern);
         try{
-            Date trono = parser.parse(hora);
-            return trono;
+            Date crono = parser.parse(fechaHora);
+            return crono;
         } catch (ParseException e) {
-            LOGGER.info("ERROR | Error Parse en: "+e);
+            log.error("ERROR | Error Parse en: "+e);
+            return null;
+        }
+    }
+
+    public synchronized String DateToString(String pattern, Date fechaHora){
+        SimpleDateFormat parser = new SimpleDateFormat(pattern);
+
+        try {
+            String crono = parser.format(fechaHora);
+            return crono;
+        } catch (Exception e) {
+            log.error("ERROR | Error Parse en: "+e);
+            return null;
+        }
+    }
+
+    public synchronized Date DateSuma(Date fechaHora, int dias){
+        Calendar c = Calendar.getInstance();
+        c.setTime(fechaHora);
+        c.add(Calendar.DATE, dias);
+        return c.getTime();
+    }
+
+
+    public synchronized String DateSuma(String pattern,Date fechaHora, int dias){
+        SimpleDateFormat parser = new SimpleDateFormat(pattern);
+
+        try {
+            Calendar c = Calendar.getInstance();
+            c.setTime(fechaHora);
+            c.add(Calendar.DATE, dias);
+            Date crono = c.getTime();
+            return parser.format(crono);
+        } catch (Exception e) {
+            log.error("ERROR | Error Parse en: "+e);
             return null;
         }
     }
