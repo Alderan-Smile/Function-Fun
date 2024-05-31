e53258e3ef87986b8d5cf025b39ab9f17c100dc7
Oliver Consterla Araya <oliver_consterla@yahoo.cl>
2024-05-31 18:38:23 -0400

    chore: Update dependencies in pom.xml and refactor FunDate class

--- src/main/java/function_fun/DateAndTime/FunDate.java
+++ src/main/java/function_fun/DateAndTime/FunDate.java
@@ -1,4 +1,4 @@
-package DateAndTime;
+package function_fun.DateAndTime;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +17,13 @@ import java.util.Date;
 @Slf4j
 public class FunDate {
 
+    /**
+    * Converts a string representation of a date and time to a Date object using the specified pattern.
+    *
+    * @param pattern    the pattern used to parse the string representation of the date and time
+    * @param fechaHora  the string representation of the date and time
+    * @return           the Date object representing the parsed date and time, or null if parsing fails
+    */
     public synchronized Date StringToDate(String pattern, String fechaHora){
         SimpleDateFormat parser = new SimpleDateFormat(pattern);
         try{
@@ -28,6 +35,13 @@ public class FunDate {
         }
     }
 
+    /**
+     * Converts a Date object to a formatted string representation using the specified pattern.
+     *
+     * @param pattern the pattern to be used for formatting the date
+     * @param fechaHora the Date object to be converted to a string
+     * @return the formatted string representation of the date, or null if an error occurs during parsing
+     */
     public synchronized String DateToString(String pattern, Date fechaHora){
         SimpleDateFormat parser = new SimpleDateFormat(pattern);
 
@@ -40,6 +54,13 @@ public class FunDate {
         }
     }
 
+    /**
+    * Calculates the sum of a given number of days to a given date.
+    *
+    * @param fechaHora the date to which the days will be added
+    * @param dias the number of days to be added
+    * @return the resulting date after adding the specified number of days
+    */
     public synchronized Date DateSuma(Date fechaHora, int dias){
         Calendar c = Calendar.getInstance();
         c.setTime(fechaHora);
@@ -48,6 +69,14 @@ public class FunDate {
     }
 
 
+    /**
+     * Adds the specified number of days to the given date and returns the result as a formatted string.
+     *
+     * @param pattern the pattern to be used for formatting the date
+     * @param fechaHora the date to which the days should be added
+     * @param dias the number of days to be added
+     * @return the formatted string representing the resulting date
+     */
     public synchronized String DateSuma(String pattern,Date fechaHora, int dias){
         SimpleDateFormat parser = new SimpleDateFormat(pattern);
 
